e0258144782accf51ad8c57e582d81f8568bbfb6
Athena <athena.partch@gmail.com>
2021-01-28 16:02:03 -0800

    add token in JS and html file

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+// Stream Chat server SDK
+const StreamChat = require('stream-chat').StreamChat;
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.sendFile('/index.html');
+});
+
+app.listen(8800, () => {
+  console.log('Example app listening on port 8800!');
+});
+
+// [...]
+
+const serverClient = new StreamChat('<S36thr2wa5bn5>', '<jym4npntsfk3bujzwe2tpy7fg44vnv9zdwmsa4c4j8nus3633p7mfpc8nmdtd87b>');
+
+app.get('/token', (req, res) => {
+  const { username } = req.query;
+  if (username) {
+    const token = serverClient.createToken(username);
+    res.status(200).json({ token, status: 'sucess' });
+  } else {
+    res.status(401).json({ message: 'invalid request', status: 'error' });
+  }
+});
+
+// [...]
